13bca74212fac4eafb61b86c6af7c9ae2cef21d8
nikhil <sg804595@gmail.com>
2025-01-20 21:28:35 +0530

    routes completed

--- src/routes/mockTestRoutes.ts
+++ src/routes/mockTestRoutes.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 import { generateMockTest, submitMockTest  } from '../controllers/mockTestController';
 
 const router: Router = express.Router();
-
+// routes fot mock
 router.get('/:userId', generateMockTest);  
 
 router.post('/submit', submitMockTest);
--- src/routes/questionRoutes.ts
+++ src/routes/questionRoutes.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 import { createQuestion } from '../controllers/questionController';
 
 const router: Router = express.Router();
-
+// routes for question 
 router.post('/', createQuestion as express.RequestHandler);
 
 export { router as questionRoutes };
